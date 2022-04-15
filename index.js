94bbc6338b22592a1f5133f9664263c7940a6eac
jambackie <jambackie@gmail.com>
2022-04-15 17:38:13 +0300

    main adaptive

---
+++ index.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector('.nav-collapse')
+const toggleIcon = document.querySelector('.nav-toggle')
+const mobileMenu = document.querySelector('.menu-mobile')
+const mobileLinks = document.querySelectorAll('.menu-mobile a')
+
+function openMenu () {
+  toggleIcon.classList.remove('nav-open')
+  toggleIcon.classList.add('nav-close')
+  mobileMenu.classList.remove('menu-hidden')
+}
+
+function closeMenu () {
+  toggleIcon.classList.remove('nav-close')
+  toggleIcon.classList.add('nav-open')
+  mobileMenu.classList.add('menu-hidden')
+}
+
+toggleBtn.addEventListener('click', () => {
+  toggleIcon.classList.contains('nav-open') ? openMenu() : closeMenu()
+})
+
+mobileLinks.forEach(el => el.addEventListener('click', closeMenu))
+console.log(toggleBtn)
